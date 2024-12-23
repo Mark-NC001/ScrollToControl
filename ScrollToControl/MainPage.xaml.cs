db2ab8a5cdb86cfb5559125fb37087a81d5c216e
Mark <75133432+Mark-NC001@users.noreply.github.com>
2024-12-23 12:50:14 +0000

    Add files via upload

---
+++ ScrollToControl/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace ScrollToControl
+{
+	public partial class MainPage : ContentPage
+	{
+
+		private bool _firstTime = true;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private async void TopButton_Clicked(object sender, EventArgs e)
+		{
+			await MyScrollView.ScrollToAsync(BottomButton, ScrollToPosition.MakeVisible, true);
+		}
+
+		private async void BottomButton_Clicked(object sender, EventArgs e)
+		{
+			await MyScrollView.ScrollToAsync(TopButton, ScrollToPosition.MakeVisible, true);
+		}
+
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+
+			if (_firstTime)
+			{
+					await MyScrollView.ScrollToAsync(BottomButton, ScrollToPosition.MakeVisible, true);
+				_firstTime = false;
+			}
+
+		}
+
+	}
+
+}
